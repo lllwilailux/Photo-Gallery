1d0c04e696af3541296ad78b2f12754e271d3f1e
wilailux <lllwilailux@gmail.com>
2016-08-18 13:59:27 +0700

    update

--- app/src/main/java/com/augmentis/ayp/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/augmentis/ayp/photogallery/PhotoGalleryFragment.java
@@ -1,15 +1,20 @@
 package com.augmentis.ayp.photogallery;
 
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -53,16 +58,21 @@ public class PhotoGalleryFragment extends Fragment{
 
     class PhotoGalleryVH extends RecyclerView.ViewHolder {
 
-        TextView mText;
+//        TextView mText;
+        ImageView mPhoto;
 
         public PhotoGalleryVH(View itemView) {
             super(itemView);
-            mText = (TextView) itemView;
+            mPhoto = (ImageView) itemView.findViewById(R.id.image_photo);
         }
 
-        public void bindGalleryItem(GalleryItem galleryItem) {
-            mText.setText(galleryItem.getTitle());
+        public void bindDrawable(@NonNull Drawable drawable) {
+            mPhoto.setImageDrawable(drawable);
         }
+
+//        public void bindGalleryItem(GalleryItem galleryItem) {
+//            mText.setText(galleryItem.getTitle());
+//        }
     }
 
     class PhotoGalleryAdapter extends RecyclerView.Adapter<PhotoGalleryVH> {
@@ -75,13 +85,19 @@ public class PhotoGalleryFragment extends Fragment{
 
         @Override
         public PhotoGalleryVH onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(getActivity()).inflate(android.R.layout.simple_list_item_1,parent,false);
+            View v = LayoutInflater.from(getActivity()).inflate(R.layout.item_photo,parent,false);
             return new PhotoGalleryVH(v);
         }
 
         @Override
         public void onBindViewHolder(PhotoGalleryVH holder, int position) {
-            holder.bindGalleryItem(mGalleryItemList.get(position));
+//            holder.bindGalleryItem(mGalleryItemList.get(position));
+            Drawable smileyDrawable = ResourcesCompat.getDrawable(getResources(),R.drawable.bear,null);
+
+            GalleryItem galleryItem = mGalleryItemList.get(position);
+            Log.d(TAG,"bind position # " + position + " , url " + galleryItem.getUrl());
+
+            holder.bindDrawable(smileyDrawable);
         }
 
         @Override
@@ -99,6 +115,14 @@ public class PhotoGalleryFragment extends Fragment{
             mFlickrFetcher.fetchItems(itemList);
             Log.d(TAG,"Fetcher task finished");
             return itemList;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+
+            String formatString = getResources().getString(R.string.photo_progress_loaded);
+            Snackbar.make(mRecyclerView,formatString,Snackbar.LENGTH_SHORT).show();
         }
 
         @Override
